7a8c9b583491e63bae06d1d91eefe3df0cb1a4a6
erfan hosseini <erfanhosseini768@yahoo.com>
2025-02-26 22:18:28 +0330

    create template-component last products

---
+++ src/components/templates/index/latest/Latest.js
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import React from 'react'
+import styles from "./Latest.module.css"
+import { FaChevronLeft } from "react-icons/fa6";
+import Product from "@/components/modules/product/Product";
+
+export default function Latest() {
+  return (
+    <div className={styles.container}>
+      <section className={styles.title}>
+        <div>
+          <p>آخرین محصولات</p>
+          <span>Latest products</span>
+        </div>
+        <Link className={styles.link_btn} href={"/category"}>
+          مشاهده همه <FaChevronLeft />{" "}
+        </Link>
+      </section>
+      <main data-aos="fade-up" className={styles.products}>
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+      </main>
+    </div>
+  )
+}
