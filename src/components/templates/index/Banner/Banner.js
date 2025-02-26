60bfe47515c3ce20a569848924883485de2ffdcd
erfan hosseini <erfanhosseini768@yahoo.com>
2025-02-26 10:26:10 +0330

    create template-component banner

---
+++ src/components/templates/index/Banner/Banner.js
@@ -0,0 +1,51 @@
+"use client";
+import React from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+// import required modules
+import { Navigation, Autoplay } from "swiper/modules";
+
+function Banner() {
+  return (
+    <Swiper
+      rewind={true}
+      navigation={true}
+      loop={true}
+      autoplay={{ delay: 3500 }}
+      modules={[Navigation, Autoplay]}
+      className="mySwiper home-slider"
+    >
+      <SwiperSlide>
+      <img
+          src="https://set-coffee.com/wp-content/uploads/2021/10/winter-slie.jpg"
+          alt="Slide"
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+          src="https://set-coffee.com/wp-content/uploads/2022/06/82.jpg"
+          alt="Slide"
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+          src="https://set-coffee.com/wp-content/uploads/2024/08/slider4.jpeg"
+          alt="Slide"
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+          src="https://set-coffee.com/wp-content/uploads/2024/12/b-and-rw-slide.jpg"
+          alt="Slide"
+        />
+      </SwiperSlide>
+    </Swiper>
+  );
+}
+
+export default Banner;
