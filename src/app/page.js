2bb8b89999fd681cf0764aaf682ba29dc2b93688
erfan hosseini <erfanhosseini768@yahoo.com>
2025-02-27 09:07:10 +0330

    import components

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 import Navbar from "@/components/modules/navbar/Navbar";
 import Banner from "@/components/templates/index/Banner/Banner";
 import Latest from "@/components/templates/index/latest/Latest";
+import About from "@/components/templates/index/about/About";
+import Articles from "@/components/templates/index/articles/Articles";
 
 export default function Home() {
   return (
@@ -8,6 +10,8 @@ export default function Home() {
       <Navbar />
       <Banner />
       <Latest />
+      <About />
+      <Articles /> 
     </>
   );
 }
