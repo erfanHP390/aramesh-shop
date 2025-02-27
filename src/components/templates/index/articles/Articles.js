be8677683688cffc084cdbf57eba4618a9b4a09d
erfan hosseini <erfanhosseini768@yahoo.com>
2025-02-27 09:05:04 +0330

    create template-component articles

---
+++ src/components/templates/index/articles/Articles.js
@@ -0,0 +1,77 @@
+// Articles.js
+"use client";
+import React from 'react';
+import "./articles.module.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Autoplay, Navigation } from "swiper/modules";
+import Article from '@/components/modules/article/Article';
+import styles from "./articles.module.css";
+
+export default function Articles() {
+  return (
+    <div className={styles.container}>
+      <p className={styles.title}>مقالات ما</p>
+      <span className={styles.description}>دانستنی های جذاب دنیای قهوه</span>
+      <main>
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={30}
+          dir="rtl"
+          // autoplay={{ delay: 1500, disableOnInteraction: false }}
+          loop={true}
+          navigation={true}
+          modules={[Navigation, Autoplay]}
+          className="mySwiper articles_slider"
+          breakpoints={{
+            320: {
+              slidesPerView: 1,
+              spaceBetween: 10,
+            },
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            986: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 30,
+            },
+          }}
+        >
+          <SwiperSlide>
+            <Article />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Article />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Article />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Article />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Article />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Article />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Article />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Article />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Article />
+          </SwiperSlide>
+        </Swiper>
+      </main>
+    </div>
+  );
+}
