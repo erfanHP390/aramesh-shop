9406d5f985c845cfaa7cc6c884a2ddc0cd456ab0
erfan hosseini <erfanhosseini768@yahoo.com>
2025-03-01 19:57:18 +0330

    import mod-compo footer

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Banner from "@/components/templates/index/Banner/Banner";
 import Latest from "@/components/templates/index/latest/Latest";
 import About from "@/components/templates/index/about/About";
 import Articles from "@/components/templates/index/articles/Articles";
+import Footer from "@/components/modules/Footer/Footer";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <Latest />
       <About />
       <Articles /> 
+      <Footer />
     </>
   );
 }
