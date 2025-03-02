570d1c086b55720f0311b3afb485358f2ab6b010
erfan hosseini <erfanhosseini768@yahoo.com>
2025-03-02 23:43:47 +0330

    create model for users

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+
+const schema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: false
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: false
+    },
+    role: {
+        type: String,
+        default: "USER"
+    },
+
+    // refreshToken: String
+
+} , {timestamps: true}) 
+
+
+const model = mongoose.models.USER || mongoose.model("USER" , schema)
+export default model
