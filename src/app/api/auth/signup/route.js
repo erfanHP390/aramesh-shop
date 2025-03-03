05ffbf9010c879ae3211e326d34a808091465623
erfan hosseini <erfanhosseini768@yahoo.com>
2025-03-03 22:33:31 +0330

    validations for sign-up

--- src/app/api/auth/signup/route.js
+++ src/app/api/auth/signup/route.js
@@ -2,42 +2,84 @@ import connectToDB from "@/configs/db";
 import UserModel from "@/models/User"
 import { generateAccessToken, hashPassword } from "@/utils/auth";
 import { roles } from "@/utils/constants";
+import { validatePhone , validateEmail ,validatePassword } from "@/utils/auth";
+
 
 export async function POST(req) {
 
     connectToDB()
 
-    const body = await req.json()
-    const {name , phone , email , password} = body
+    try {
+        const body = await req.json()
+        const {name , phone , email , password} = body
+    
+        if(!name.trim()) {
+            return Response.json({message: "name have not empty"} , {
+                status: 400
+            })
+        }
+    
+        const isValidPhone = validatePhone(phone)
+        if(!isValidPhone) {
+        return  Response.json({message: "phone format is not valid"} , {
+            status: 406
+        })
+        }
 
-    const isUserExist = await UserModel.findOne({
-        $or: [{name} , {email} , {phone}]
-    })
+        if(email) {
+            const isValidEmail = validateEmail(email)
+      
+            if(!isValidEmail) {
+             return Response.json({message: "email is not valid"} , {
+                status: 412
+             })
+            }
+          }
 
-    if(isUserExist) {
-        return Response.json({message: "the username or email or phone is already exist"} , {
-            status: 422
+          if(password) {
+            const isValidPassword = validatePassword(password)
+          
+            if(!isValidPassword) {
+             return Response.json({message: "password is not valid"} , {
+                status: 409
+             })
+            }
+          }
+    
+        const isUserExist = await UserModel.findOne({
+            $or: [{name} , {email} , {phone}]
+        })
+    
+        if(isUserExist) {
+            return Response.json({message: "the username or email or phone is already exist"} , {
+                status: 422
+            })
+        }
+    
+        const hashedPassword = await hashPassword(password)
+        const accessToken = generateAccessToken({name})
+    
+        const users = await UserModel.find({})
+    
+        await UserModel.create({
+            name,
+            phone,
+            email,
+            password: hashedPassword,
+            role: users.length > 0 ? roles.USER : roles.ADMIN
+        })
+    
+        return Response.json({message: "user signed up is successfully"} , {
+            status: 201,
+            headers: {
+                "Set-Cookie": `token=${accessToken};path=/;httpOnly=true`
+            }
+        })
+    
+    }catch (err) {
+        return Response.json({message: `enterval error server => ${err}`} , {
+            status: 500
         })
     }
-
-    const hashedPassword = await hashPassword(password)
-    const accessToken = generateAccessToken({name})
-
-    const users = await UserModel.find({})
-
-    await UserModel.create({
-        name,
-        phone,
-        email,
-        password: hashedPassword,
-        role: users.length > 0 ? roles.USER : roles.ADMIN
-    })
-
-    return Response.json({message: "user signed up is successfully"} , {
-        status: 201,
-        headers: {
-            "Set-Cookie": `token=${accessToken};path=/;httpOnly=true`
-        }
-    })
 
 }
--- src/components/templates/login&register/Register.js
+++ src/components/templates/login&register/Register.js
@@ -3,7 +3,9 @@ import styles from "./Register.module.css";
 import Link from "next/link";
 import Sms from "./Sms";
 import { Bounce, toast } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
+import { toastSuccess, toastError ,  swalAlert} from "@/utils/helpers";
+import { validatePhone , validateEmail ,validatePassword } from "@/utils/auth";
 
 const Register = ({ showLoginForm }) => {
   const [registerWithPass, setRegisterWithPass] = useState(false);
@@ -12,76 +14,108 @@ const Register = ({ showLoginForm }) => {
   const [phone, setPhone] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [isLoading , setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
 
   const hideOtpForm = () => setIsRegisterWithOtp(false);
 
   const signup = async () => {
-    
-    const newUser = {
-      name,phone,email,password
+
+    if(!name.trim()) {
+      setIsLoading(false)
+      return swalAlert("نام نمی تواند خالی باشد" , "error" , "تلاش مجدد")
     }
 
-    const res = await fetch("/api/auth/signup" , {
+    const isValidPhone = validatePhone(phone)
+    if(!isValidPhone) {
+      setIsLoading(false)
+    return  swalAlert("شماره تماس  معتبر است" , "error" , "تلاش مجدد")
+    }
+
+    if(email) {
+      const isValidEmail = validateEmail(email)
+
+      if(!isValidEmail) {
+        setIsLoading(false)
+       return swalAlert("ایمیل نامعتبر است" , "error" , "تلاش مجدد")
+      }
+    }
+
+    if(password) {
+      const isValidPassword = validatePassword(password)
+    
+      if(!isValidPassword) {
+        setIsLoading(false)
+       return swalAlert("رمز عبور نا معتبر است.رمز عبور باید شامل حداقل یک کاراکتر،یک حرف بزرگ و حرف کوچک و عدد باشد" , "error" , "تلاش مجدد")
+      }
+    }
+
+    const newUser = {
+      name,
+      phone,
+      email,
+      password,
+    };
+
+    const res = await fetch("/api/auth/signup", {
       method: "POST",
       headers: {
-        "Content-Type" : "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(newUser)
-    })
+      body: JSON.stringify(newUser),
+    });
 
-
-    if(res.status === 201) {
-      setName("")
-      setPhone("")
-      setEmail("")
-      setPassword("")
-      setIsLoading(false)
-      toast.success('ثبت نام با موفقیت انجام شد', {
-        position: "top-center",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "colored",
-      })
+    if (res.status === 201) {
+      setName("");
+      setPhone("");
+      setEmail("");
+      setPassword("");
+      setIsLoading(false);
+      toastSuccess(
+        "ثبت نام با موفقیت انجام شد",
+        "top-center",
+        5000,
+        false,
+        true,
+        true,
+        true,
+        undefined,
+        "colored"
+      );
     } else if (res.status === 422) {
-      setName("")
-      setPhone("")
-      setEmail("")
-      setPassword("")
-      setIsLoading(false)
-      toast.error('نام/شماره تلفن / ایمیل شما قبلا ثبت شده است لطفا دوباره اقدام کنید', {
-        position: "top-center",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "colored",
-        });
-    } else if(res.status === 500) {
-      setName("")
-      setPhone("")
-      setEmail("")
-      setPassword("")
-      setIsLoading(false)
-      toast.error('خطا در سرور لطفا بعدا تلاش کنید', {
-        position: "top-center",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "colored",
-        });
+      setName("");
+      setPhone("");
+      setEmail("");
+      setPassword("");
+      setIsLoading(false);
+      toastError(
+        "نام/شماره تلفن / ایمیل شما قبلا ثبت شده است لطفا دوباره اقدام کنید",
+        "top-center",
+        5000,
+        false,
+        true,
+        true,
+        true,
+        undefined,
+        "colored"
+      );
+    } else if (res.status === 500) {
+      setName("");
+      setPhone("");
+      setEmail("");
+      setPassword("");
+      setIsLoading(false);
+      toastError(
+        "خطا در سرور ، لطفا بعدا تلاش کنید",
+        "top-center",
+        5000,
+        false,
+        true,
+        true,
+        true,
+        undefined,
+        "colored"
+      );
     }
-    
-    
   };
 
   return (
@@ -131,22 +165,20 @@ const Register = ({ showLoginForm }) => {
               ثبت نام با کد تایید
             </p>
 
-                <button
-                  onClick={() => {
-                    if(registerWithPass) {
-                      setIsLoading(true)
-                      signup()
-                    } else {
-                      setRegisterWithPass(true)
-                    }
-                  }}
-                  style={{ marginTop: ".7rem" }}
-                  className={styles.btn}
-                >
-                  {
-                    isLoading ? "لطفا صبر کنید..." : "ثبت نام با رمز عبور"
-                  }
-                </button>
+            <button
+              onClick={() => {
+                if (registerWithPass) {
+                  setIsLoading(true);
+                  signup();
+                } else {
+                  setRegisterWithPass(true);
+                }
+              }}
+              style={{ marginTop: ".7rem" }}
+              className={styles.btn}
+            >
+              {isLoading ? "لطفا صبر کنید..." : "ثبت نام با رمز عبور"}
+            </button>
             <p onClick={showLoginForm} className={styles.back_to_login}>
               برگشت به ورود
             </p>
