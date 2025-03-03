3d6a9b6403aec507e3786f085518fd1f2eca391c
erfan hosseini <erfanhosseini768@yahoo.com>
2025-03-03 12:22:17 +0330

    validation info for sign-up/in

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -35,10 +35,28 @@ const generateRefreshToken = (data) => {
   return token;
 };
 
+const validatePhone = (phone) => {
+  const regexPhone = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/g
+  return regexPhone.test(phone)
+}
+
+const validateEmail = (email) => {
+  const regexEmail = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/g
+  return regexEmail.test(email)
+}
+
+const validatePassword = (password) => {
+  const regexPass = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/g
+  return regexPass.test(password)
+}
+
 export {
   hashPassword,
   verifyPassword,
   generateAccessToken,
   verifyToken,
   generateRefreshToken,
+  validatePhone,
+  validateEmail,
+  validatePassword
 };
