2295f83074180ac8de3a4ef27e54b80af3646aac
erfan hosseini <erfanhosseini768@yahoo.com>
2025-03-05 19:16:44 +0330

    verify token to show links in navbar

--- src/app/page.js
+++ src/app/page.js
@@ -4,20 +4,33 @@ import Latest from "@/components/templates/index/latest/Latest";
 import About from "@/components/templates/index/about/About";
 import Articles from "@/components/templates/index/articles/Articles";
 import Footer from "@/components/modules/Footer/Footer";
-import { authUser } from "@/utils/auth";
+import { cookies } from "next/headers";
+import UserModel from "@/models/User"
+import connectToDB from "@/configs/db";
+import { verifyToken } from "@/utils/auth";
 
 export default async function Home() {
 
-  const user = await authUser()
+  connectToDB()
 
-  return (
-    <>
-      <Navbar  isLogin={user} />
-      <Banner />
-      <Latest />
-      <About />
-      <Articles /> 
-      <Footer />
-    </>
-  );
+  const token = cookies().get("token")
+  let user = null
+
+  if (token) {
+    const tokenPayload = verifyToken(token.value);
+    if (tokenPayload) {
+      user = await UserModel.findOne({ email: tokenPayload.email });
+    }
+  }
+
+return (
+<>
+<Navbar isLogin={user} />
+<Banner />
+<Latest />
+<About />
+<Articles />
+<Footer />
+</>
+);
 }
