0a9b9c24e9db2a2b7e611bae3ec507c29245eade
erfan hosseini <erfanhosseini768@yahoo.com>
2025-03-05 19:17:59 +0330

    create page not-found

---
+++ src/app/not-found.js
@@ -0,0 +1,19 @@
+import styles from "@/styles/404.module.css"
+import Link from 'next/link'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <div className={styles.contents}>
+        <p className={styles.left_number}>4</p>
+        <div className={styles.mug}></div>
+        <p className={styles.right_number}>4</p>
+      </div>
+      <div className={styles.texts}>
+        <p>صفحه مورد نظر یافت نشد :((</p>
+        <a href="/">برگشت به خانه</a>
+      </div>
+    </div>
+  )
+}
