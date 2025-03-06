bf40585f5eac510e0f4ec8996b9c0c23181b965e
erfan hosseini <erfanhosseini768@yahoo.com>
2025-03-06 11:25:31 +0330

    create model for comments

---
+++ models/Comment.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose")
+require("./Product")
+
+const schema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    } ,
+    email: {
+        type: String,
+        required: true
+    } , 
+    score: {
+        type: Number,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: () => Date.now(),
+        immutable: false
+    },
+    product: {
+        type: mongoose.Types.ObjectId,
+        ref: "Product"
+    }
+})
+
+
+const model = mongoose.models.Comment || mongoose.model("Comment" , schema)
+
+export default model
