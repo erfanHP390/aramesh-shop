06e5487f0e5f8a0ffcd1fb1a3a82ac74c4ff17db
erfan hosseini <erfanhosseini768@yahoo.com>
2025-03-07 21:37:08 +0330

    create post-route for wishlist

---
+++ src/app/api/whishlist/route.js
@@ -0,0 +1,61 @@
+import connectToDB from "@/configs/db";
+import WishlistModel from "@/models/Wishlist";
+import UserModel from "@/models/User";
+import ProductModel from "@/models/Product";
+import mongoose from 'mongoose';
+
+export async function POST(req) {
+    connectToDB();
+
+    try {
+        const body = await req.json();
+        const { user, product } = body;
+
+        if (!user || !product) {
+            return Response.json({ message: "user/product must have something" }, {
+                status: 400
+            });
+        }
+
+        // Use 'new' with ObjectId
+        const userId = mongoose.Types.ObjectId.isValid(user) ? new mongoose.Types.ObjectId(user) : null;
+        const productId = mongoose.Types.ObjectId.isValid(product) ? new mongoose.Types.ObjectId(product) : null;
+
+        if (!userId) {
+            return Response.json({ message: "Invalid user ID" }, {
+                status: 422
+            });
+        }
+
+        if (!productId) {
+            return Response.json({ message: "Invalid product ID" }, {
+                status: 422
+            });
+        }
+
+        const isValidUser = await UserModel.findOne({ _id: userId });
+        if (!isValidUser) {
+            return Response.json({ message: "user not found!" }, {
+                status: 404
+            });
+        }
+
+        const isValidProduct = await ProductModel.findOne({ _id: productId });
+        if (!isValidProduct) {
+            return Response.json({ message: "product not found!" }, {
+                status: 404
+            });
+        }
+
+        await WishlistModel.create({ user: userId, product: productId });
+
+        return Response.json({ message: "product added wishlist successfully" }, {
+            status: 201
+        });
+
+    } catch (err) {
+        return Response.json({ message: `interval error server: ${err}` }, {
+            status: 500
+        });
+    }
+}
