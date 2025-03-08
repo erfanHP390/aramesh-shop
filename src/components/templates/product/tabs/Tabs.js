5322749171aed55e96e83be5c5c9cd21911380c0
erfan hosseini <erfanhosseini768@yahoo.com>
2025-03-08 11:26:51 +0330

    change code and styles for better UI performance

--- src/components/templates/product/tabs/Tabs.js
+++ src/components/templates/product/tabs/Tabs.js
@@ -9,57 +9,52 @@ const Tabs = ({product}) => {
     const [tab, setTab] = useState("description");
 
     return (
+      <>
         <div data-aos="fade-left" className={styles.tabs}>
-            <input
+          <ul>
+            <li>
+              <button
+                className={tab === "description" ? styles.active_tab : ""}
                 onClick={() => setTab("description")}
-                type="radio"
-                id="description"
-                name="tab-control"
-                checked={tab === "description"}
-            />
-            <input
+              >
+                توضیحات
+              </button>
+            </li>
+            <li>
+              <button
+                className={tab === "moreInfoes" ? styles.active_tab : ""}
                 onClick={() => setTab("moreInfoes")}
-                type="radio"
-                id="moreInfoes"
-                name="tab-control"
-                checked={tab === "moreInfoes"}
-            />
-            <input
+              >
+                اطلاعات بیشتر
+              </button>
+            </li>
+            <li>
+              <button
+                className={tab === "comments" ? styles.active_tab : ""}
                 onClick={() => setTab("comments")}
-                type="radio"
-                id="comments"
-                name="tab-control"
-                checked={tab === "comments"}
-            />
-            <ul>
-                <li title="Features">
-                    <label htmlFor="description" role="button">
-                        توضیحات
-                    </label>
-                </li>
-                <li title="Delivery Contents">
-                    <label htmlFor="moreInfoes" role="button">
-                        اطلاعات بیشتر
-                    </label>
-                </li>
-                <li title="Shipping">
-                    <label htmlFor="comments" role="button">
-                        نظرات ({product.comments.filter(comment => comment.isAccept).length})
-                    </label>
-                </li>
-            </ul>
-            <div className={styles.contents}>
-                <section className={styles.tabs_content}>
-                    <Description />
-                </section>
-                <section className={styles.tabs_content}>
-                    <MoreInfoes  product={JSON.parse(JSON.stringify(product))} />
-                </section>
-                <section className={styles.tabs_content}>
-                    <Comments  comments={JSON.parse(JSON.stringify(product.comments))}  productID={JSON.parse(JSON.stringify(product._id))} />
-                </section>
-            </div>
+              >
+                نظرات (
+                {product.comments.filter((comment) => comment.isAccept).length})
+              </button>
+            </li>
+          </ul>
+  
+          <div className={styles.contents}>
+            <section>
+              {tab === "description" && <Description />}
+              {tab == "moreInfoes" && (
+                <MoreInfoes product={JSON.parse(JSON.stringify(product))} />
+              )}
+              {tab == "comments" && (
+                <Comments
+                  productID={product._id}
+                  comments={JSON.parse(JSON.stringify(product.comments))}
+                />
+              )}
+            </section>
+          </div>
         </div>
+      </>
     );
 };
 
