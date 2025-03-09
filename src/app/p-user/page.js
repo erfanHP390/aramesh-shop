1bab88bd43e5f7cc7a48477372c8be6b55976295
erfan hosseini <erfanhosseini768@yahoo.com>
2025-03-09 23:52:37 +0330

    create page-user-panel

---
+++ src/app/p-user/page.js
@@ -0,0 +1,29 @@
+import UserPanelLayout from '@/components/layouts/UserPanelLayout/UserPanelLayout'
+import Box from '@/components/templates/p-user/index/box/Box'
+import Orders from '@/components/templates/p-user/index/orders/Orders'
+import Tickets from '@/components/templates/p-user/index/tickets/Tickets'
+import React from 'react'
+import styles from "@/styles/pUser.module.css"
+
+function Index() {
+  return (
+    <>
+      <UserPanelLayout>
+      <main>
+        <section className={styles.boxes}>
+          <Box title="مجموع تیکت ها " value="20" />
+          <Box title="مجموع کامنت ها " value="0" />
+          <Box title="مجموع سفارشات" value="2" />
+          <Box title="مجموع علاقه مندی ها" value="10" />
+        </section>
+        <section className={styles.contents}>
+          <Tickets />
+          <Orders />
+        </section>
+      </main>
+      </UserPanelLayout>
+    </>
+  )
+}
+
+export default Index
