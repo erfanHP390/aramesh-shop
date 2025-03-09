b3c95a5e3c151e2befdc5717dcb1eaef4b8b43c6
erfan hosseini <erfanhosseini768@yahoo.com>
2025-03-10 00:00:23 +0330

    create p-user template-component tickets

---
+++ src/components/templates/p-user/index/tickets/Tickets.js
@@ -0,0 +1,24 @@
+import Ticket from "../ticket/Ticket";
+import styles from "./Tickets.module.css";
+import Link from "next/link";
+import { FaArrowLeft } from "react-icons/fa";
+
+const Tickets = () => {
+  return (
+    <div className={styles.content}>
+      <div className={styles.content_details}>
+        <p>تیکت های اخیر</p>
+        <Link href="/p-user/tickets">
+          همه تیکت ها <FaArrowLeft />
+        </Link>
+      </div>
+      <div className={styles.ticket_container}>
+        <Ticket />
+        <Ticket />
+        <Ticket />
+      </div>
+    </div>
+  );
+};
+
+export default Tickets;
