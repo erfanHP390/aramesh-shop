82b59754574017065757e20cac2b1b2e5f02a2f9
erfan hosseini <erfanhosseini768@yahoo.com>
2025-03-11 00:25:30 +0330

    create page user-list-comments

---
+++ src/app/p-user/comments/page.js
@@ -0,0 +1,30 @@
+import DataTable from "@/components/templates/p-user/comments/DataTable"
+import UserPanelLayout from "@/components/layouts/UserPanelLayout/UserPanelLayout"
+import connectToDB from "@/configs/db"
+import CommentModel from "@/models/Comment"
+import { authUser } from "@/utils/authUserLink"
+
+
+async function page() {
+
+    connectToDB()
+    const user = await authUser()
+    const comments = await CommentModel.find({user : String(user._id)} , "-__v").populate("productID" , "name")
+
+    
+
+  return (
+    <UserPanelLayout>
+            <main>
+        <DataTable  comments={JSON.parse(JSON.stringify(comments))}
+          title="لیست کامنت‌ها"
+        />
+        {/* <p className={styles.empty}>
+          کامنتی وجود ندارد
+        </p>  */}
+      </main>
+    </UserPanelLayout>
+  )
+}
+
+export default page
