934bf8e30bb4f439180aa17581b38ea0e8a4348c
erfan hosseini <erfanhosseini768@yahoo.com>
2025-03-11 19:19:08 +0330

    create model for department

---
+++ models/Department.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+
+const schema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    }
+})
+
+const model = mongoose.models.Department || mongoose.model("Department" , schema)
+export default model
