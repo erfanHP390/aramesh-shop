53968ebfdc14176030bc4f3004391be93b0588fd
erfan hosseini <erfanhosseini768@yahoo.com>
2025-03-11 23:26:51 +0330

    create route-get for sub-departments of departments

---
+++ src/app/api/departments/sub/[id]/route.js
@@ -0,0 +1,30 @@
+import connectToDB from "@/configs/db";
+import { isValidObjectId } from "mongoose";
+import SubDepartmentsModel from "@/models/SubDepartment";
+
+export async function GET(req, { params }) {
+  try {
+    connectToDB();
+    const id = params.id;
+
+    if (!isValidObjectId(id)) {
+      return Response.json(
+        { message: "ID is not valid" },
+        {
+          status: 422,
+        }
+      );
+    }
+
+    const subDepartments = await SubDepartmentsModel.find({ department: id });
+
+    return Response.json(subDepartments);
+  } catch (err) {
+    return Response.json(
+      { message: `error server => ${err}` },
+      {
+        status: 500,
+      }
+    );
+  }
+}
