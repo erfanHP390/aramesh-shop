d8da35675a103f082c6ea2acb06e3fe359871f29
erfan hosseini <erfanhosseini768@yahoo.com>
2025-03-11 12:12:49 +0330

    create page details-inf0-user in user-panel

---
+++ src/app/p-user/account-details/page.js
@@ -0,0 +1,13 @@
+import UserPanelLayout from '@/components/layouts/UserPanelLayout/UserPanelLayout'
+import AccountDetail from '@/components/templates/p-user/accountDetail/AccountDetail'
+
+
+function page() {
+  return (
+    <UserPanelLayout>
+      <AccountDetail />
+    </UserPanelLayout>
+  )
+}
+
+export default page
