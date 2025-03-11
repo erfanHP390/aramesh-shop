70bfa2a7c7c51b98bd0627ccc5cf4b14bd438f3d
erfan hosseini <erfanhosseini768@yahoo.com>
2025-03-11 19:20:06 +0330

    change styles for better UI

--- src/components/templates/p-user/accountDetail/AccountDetail.js
+++ src/components/templates/p-user/accountDetail/AccountDetail.js
@@ -7,185 +7,180 @@ import { swalAlert, toastError } from "@/utils/helpers";
 import { validateEmail, validatePhone } from "@/utils/auth";
 
 function AccountDetail() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
 
-  useEffect(() => {
-    const getUser = async () => {
-      const res = await fetch("/api/auth/me");
-      const data = await res.json();
+    useEffect(() => {
+        const getUser = async () => {
+            const res = await fetch("/api/auth/me");
+            const data = await res.json();
 
-      setName(data.name);
-      setEmail(data.email);
-      setPhone(data.phone);
-    };
+            setName(data.name);
+            setEmail(data.email);
+            setPhone(data.phone);
+        };
 
-    getUser();
-  }, []);
+        getUser();
+    }, []);
 
-  const updateUser = async () => {
-    const newInfo = {
-      name,
-      email,
-      phone,
-    };
+    const updateUser = async () => {
+        const newInfo = {
+            name,
+            email,
+            phone,
+        };
 
-    if(!name || !email || !phone) {
-        return swalAlert("لطفا موارد لازم را پرنمایید" , "error" , "فهمیدم")
-    }
+        if (!name || !email || !phone) {
+            return swalAlert("لطفا موارد لازم را پرنمایید", "error", "فهمیدم");
+        }
 
-    const isValidEmail = validateEmail(email)
-    const isValidPhone = validatePhone(phone)
+        const isValidEmail = validateEmail(email);
+        const isValidPhone = validatePhone(phone);
 
-    if(!isValidEmail) {
-        swalAlert("ایمیل نامعتبر است" , "error" , "فهمیدم")
-    }
+        if (!isValidEmail) {
+            swalAlert("ایمیل نامعتبر است", "error", "فهمیدم");
+        }
 
-    if(!isValidPhone) {
-        swalAlert("شماره  تلفن نامعتبر است" , "error" , "فهمیدم")
-    }
+        if (!isValidPhone) {
+            swalAlert("شماره تلفن نامعتبر است", "error", "فهمیدم");
+        }
 
-    const res = await fetch("/api/user", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newInfo),
-    });
+        const res = await fetch("/api/user", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newInfo),
+        });
 
-    if(res.status === 200) {
-        swal({
-            title: "اطلاعات با موفیقت ویرایش شد",
-            icon: "success",
-            buttons: "فهمیدم"
-        }).then(async (result) => {
-            if(result) {
-                const res = await fetch("/api/auth/signout" , {
-                    method: "POST"
-                })
+        if (res.status === 200) {
+            swal({
+                title: "اطلاعات با موفیقت ویرایش شد",
+                icon: "success",
+                buttons: "فهمیدم",
+            }).then(async (result) => {
+                if (result) {
+                    const res = await fetch("/api/auth/signout", {
+                        method: "POST",
+                    });
 
-                if(res.status === 200) {
-                    location.replace("/login&register")
+                    if (res.status === 200) {
+                        location.replace("/login&register");
+                    }
                 }
+            });
+        } else if (res.status === 400) {
+            toastError(
+                "لطفا موارد را پر کنید",
+                "top-center",
+                5000,
+                false,
+                true,
+                true,
+                true,
+                undefined,
+                "colored"
+            );
+        } else if (res.status === 422) {
+            toastError(
+                "ایمیل یا شماره تلفن نامعتبر است",
+                "top-center",
+                5000,
+                false,
+                true,
+                true,
+                true,
+                undefined,
+                "colored"
+            );
+        } else if (res.status === 500) {
+            toastError(
+                "خطا در سرور ، لطفا بعدا تلاش کنید",
+                "top-center",
+                5000,
+                false,
+                true,
+                true,
+                true,
+                undefined,
+                "colored"
+            );
+        }
+    };
 
-            }
-        })
-    } else if(res.status === 400) {
-        toastError(
-            "لطفا موارد را پر کنید",
-            "top-center",
-            5000,
-            false,
-            true,
-            true,
-            true,
-            undefined,
-            "colored"
-          );
-    } else if (res.status === 422) {
-        toastError(
-            "ایمیل یا شماره تلفن نامعتبر است",
-            "top-center",
-            5000,
-            false,
-            true,
-            true,
-            true,
-            undefined,
-            "colored"
-          );
-    } else if(res.status === 500) {
-        toastError(
-            "خطا در سرور ، لطفا بعدا تلاش کنید",
-            "top-center",
-            5000,
-            false,
-            true,
-            true,
-            true,
-            undefined,
-            "colored"
-          );
-    }
-
-    
-
-
-  };
-
-  return (
-    <main>
-      <div className={styles.details}>
-        <h1 className={styles.title}>
-          <span> جزئیات اکانت</span>
-        </h1>
-        <div className={styles.details_main}>
-          <section>
-            <div>
-              <label>نام کاربری</label>
-              <input
-                value={name}
-                onChange={(event) => setName(event.target.value)}
-                placeholder="لطفا نام کاربری خود را وارد کنید"
-                type="text"
-              />
-            </div>
-            <div>
-              <label>ایمیل</label>
-              <input
-                value={email}
-                onChange={(event) => setEmail(event.target.value)}
-                placeholder="لطفا ایمیل خود را وارد کنید"
-                type="text"
-              />
-            </div>
-            <div>
-              <label>شماره تماس</label>
-              <input
-                value={phone}
-                onChange={(event) => setPhone(event.target.value)}
-                placeholder="لطفا شماره تماس خود را وارد کنید"
-                type="number"
-              />
-            </div>
-          </section>
-          <section>
-            <div className={styles.uploader}>
-              <img src="/images/shahin.jpg" alt="" />
-              <div>
-                <div>
-                  <button>
-                    <IoCloudUploadOutline />
-                    تغییر
-                  </button>
-                  <input type="file" name="" id="" />
+    return (
+        <main>
+            <div className={styles.details}>
+                <h1 className={styles.title}>
+                    <span>جزئیات اکانت</span>
+                </h1>
+                <div className={styles.details_main}>
+                    <section>
+                        <div>
+                            <label>نام کاربری</label>
+                            <input
+                                value={name}
+                                onChange={(event) => setName(event.target.value)}
+                                placeholder="لطفا نام کاربری خود را وارد کنید"
+                                type="text"
+                            />
+                        </div>
+                        <div>
+                            <label>ایمیل</label>
+                            <input
+                                value={email}
+                                onChange={(event) => setEmail(event.target.value)}
+                                placeholder="لطفا ایمیل خود را وارد کنید"
+                                type="text"
+                            />
+                        </div>
+                        <div>
+                            <label>شماره تماس</label>
+                            <input
+                                value={phone}
+                                onChange={(event) => setPhone(event.target.value)}
+                                placeholder="لطفا شماره تماس خود را وارد کنید"
+                                type="number"
+                            />
+                        </div>
+                    </section>
+                    <section>
+                        <div className={styles.uploader}>
+                            <img src="/images/shahin.jpg" alt="" />
+                            <div>
+                                <div>
+                                    <button>
+                                        <IoCloudUploadOutline />
+                                        تغییر
+                                    </button>
+                                    <input type="file" name="" id="" />
+                                </div>
+                                <button>
+                                    <MdOutlineDelete />
+                                    حذف
+                                </button>
+                            </div>
+                        </div>
+                        <div>
+                            <label>رمز عبور</label>
+                            <div className={styles.password_group}>
+                                <input type="password" />
+                                <button>تغییر رمز عبور</button>
+                            </div>
+                        </div>
+                    </section>
                 </div>
-                <button>
-                  <MdOutlineDelete />
-                  حذف
+                <button
+                    type="submit"
+                    className={styles.submit_btn}
+                    onClick={updateUser}
+                >
+                    ثبت تغییرات
                 </button>
-              </div>
             </div>
-            <div>
-              <label>رمز عبور</label>
-              <div className={styles.password_group}>
-                <input type="password" />
-                <button>تغییر رمز عبور</button>
-              </div>
-            </div>
-          </section>
-        </div>
-        <button
-          type="submit"
-          className={styles.submit_btn}
-          onClick={updateUser}
-        >
-          ثبت تغییرات
-        </button>
-      </div>
-    </main>
-  );
+        </main>
+    );
 }
 
 export default AccountDetail;
