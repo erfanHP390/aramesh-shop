d94a2d062257d02693c91fa27726d425b4dbc1ea
erfan hosseini <erfanhosseini768@yahoo.com>
2025-03-11 21:36:23 +0330

    create route for sub-departments

---
+++ src/app/api/departments/sub/route.js
@@ -0,0 +1,44 @@
+import connectToDB from "@/configs/db"
+import SubDepartmentModel from "@/models/SubDepartment"
+import DepartmentModel from "@/models/Department"
+
+export async function POST(req) {
+
+    try {
+
+        connectToDB()
+
+        const body = await req.json()
+        const {title , department} = body
+
+        if(!title || !department) {
+            return Response.json(
+                { message: "please fill all fields" },
+                {
+                  status: 400,
+                }
+              );
+        }
+
+        const isValidDepartment = await DepartmentModel.findOne({_id: department})
+        if(!isValidDepartment) {
+            return Response.json(
+                { message: "department is not found" },
+                {
+                  status: 404,
+                }
+              );
+        }
+
+
+        await SubDepartmentModel.create({title , department})
+
+        return Response.json({message: "sub-department is created successfully"} , {
+            status: 201
+          })
+
+    } catch (err) {
+        return Response.json({message: `interval error server => ${err}`} , {status: 500})
+    }
+
+}
