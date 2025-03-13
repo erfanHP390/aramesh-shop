962461bfd81490263f038835e6895199c27a7d16
erfan hosseini <erfanhosseini768@yahoo.com>
2025-03-13 11:37:25 +0330

    convert static box to dynamic

--- src/app/p-user/page.js
+++ src/app/p-user/page.js
@@ -4,20 +4,31 @@ import Orders from '@/components/templates/p-user/index/orders/Orders'
 import Tickets from '@/components/templates/p-user/index/tickets/Tickets'
 import React from 'react'
 import styles from "@/styles/pUser.module.css"
+import connectToDB from '@/configs/db'
+import { authUser } from '@/utils/authUserLink'
+import TicketModel from "@/models/Ticket"
+import WishListModel from "@/models/Wishlist"
 
-function Index() {
+async function Index() {
+
+  connectToDB()
+  const user =await authUser()
+  const tickets = await TicketModel.find({user: user._id}).limit(3).populate("department" , "title").sort().lean()
+  const wishlist = await WishListModel.find({user: user._id})
+  const allTickets = await TicketModel.find({user: user._id})
+
   return (
     <>
       <UserPanelLayout>
       <main>
         <section className={styles.boxes}>
-          <Box title="مجموع تیکت ها " value="20" />
+          <Box title="مجموع تیکت ها " value={allTickets.length} />
           <Box title="مجموع کامنت ها " value="0" />
           <Box title="مجموع سفارشات" value="2" />
-          <Box title="مجموع علاقه مندی ها" value="10" />
+          <Box title="مجموع علاقه مندی ها" value={wishlist.length} />
         </section>
         <section className={styles.contents}>
-          <Tickets />
+          <Tickets tickets={JSON.parse(JSON.stringify(tickets))} />
           <Orders />
         </section>
       </main>
