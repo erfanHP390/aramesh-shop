6ece51fa10feed818f1b5ae0a3e3650b7f28313d
erfan hosseini <erfanhosseini768@yahoo.com>
2025-03-13 10:08:06 +0330

    difference between answer-admin or ticket-user with on field of ticket-model

--- src/app/p-user/tickets/answer/[id]/page.js
+++ src/app/p-user/tickets/answer/[id]/page.js
@@ -1,35 +1,38 @@
-import UserPanelLayout from '@/components/layouts/UserPanelLayout/UserPanelLayout'
-import Link from 'next/link'
-import styles from "@/styles/p-user/answerTicket.module.css"
-import Answer from '@/components/templates/p-user/tickets/answer/Answer'
-import connectToDB from '@/configs/db'
-import TicketModel from "@/models/Ticket"
+import UserPanelLayout from "@/components/layouts/UserPanelLayout/UserPanelLayout";
+import Link from "next/link";
+import styles from "@/styles/p-user/answerTicket.module.css";
+import Answer from "@/components/templates/p-user/tickets/answer/Answer";
+import connectToDB from "@/configs/db";
+import TicketModel from "@/models/Ticket";
 
-async function page({params}) {
+async function page({ params }) {
+  connectToDB();
+  const ticketID = params.id;
+  const ticket = await TicketModel.findOne({ _id: ticketID }).populate('user' , "name role").lean()    
 
-    connectToDB()
-    const ticketID = params.id
-    const ticket = await TicketModel.findOne({_id: ticketID})
+      const answerTicket = await TicketModel.findOne({ mainTicket: ticket._id });
 
   return (
     <UserPanelLayout>
-            <main className={styles.container}>
+      <main className={styles.container}>
         <h1 className={styles.title}>
           <span>تیکت تستی</span>
           <Link href="/p-user/tickets/sendTicket">ارسال تیکت جدید</Link>
         </h1>
 
         <div>
-          <Answer type="user" />
-          <Answer type="admin" />
+          <Answer type="user" {...ticket} />
+          {answerTicket && <Answer type="admin" />}
 
-          {/* <div className={styles.empty}>
-            <p>هنوز پاسخی دریافت نکردید</p>
-          </div> */}
+          {!answerTicket && (
+            <div className={styles.empty}>
+              <p>هنوز پاسخی دریافت نکردید</p>
+            </div>
+          )}
         </div>
       </main>
     </UserPanelLayout>
-  )
+  );
 }
 
-export default page
+export default page;
