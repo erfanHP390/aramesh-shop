42230fa4f78d5b9e97b09a91c63837ec022d0c14
erfan hosseini <erfanhosseini768@yahoo.com>
2025-03-18 08:38:06 +0330

    update user

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -28,20 +28,23 @@ export async function POST(req) {
       return Response.json({ message: "Invalid phone" }, { status: 422 });
     }
 
-
-    await UserModel.findOneAndUpdate({_id: user._id} , {
+    await UserModel.findOneAndUpdate(
+      { _id: user._id },
+      {
         $set: {
-            name,
-            email,
-            phone
-        }
-    })
+          name,
+          email,
+          phone,
+        },
+      }
+    );
 
-    return Response.json({message: "user-info updated successfully"} , {
-        status: 200
-    })
-
-
+    return Response.json(
+      { message: "user-info updated successfully" },
+      {
+        status: 200,
+      }
+    );
   } catch (err) {
     return Response.json({ message: err }, { status: 500 });
   }
