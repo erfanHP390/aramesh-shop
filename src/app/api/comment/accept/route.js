9a5f376257e1f4400ec89c92ca49660dde106214
erfan hosseini <erfanhosseini768@yahoo.com>
2025-03-19 18:11:19 +0330

    create routes for accept/reject comments

---
+++ src/app/api/comment/accept/route.js
@@ -0,0 +1,40 @@
+import connectToDB from "@/configs/db"
+import CommentModel from "@/models/Comment"
+import { isValidObjectId } from "mongoose"
+
+export async function PUT(req) {
+
+    try {
+        connectToDB()
+
+        const body = await req.json()
+        const {id} = body
+
+        if(!id) {
+            return Response.json({message: "id have send"} , {
+                status: 400
+            })
+        }
+
+        if(!isValidObjectId(id)) {
+            return Response.json({message: "id is not valid"} , {
+                status: 422
+            })            
+        }
+
+        await CommentModel.findOneAndUpdate({_id: id} , {
+            $set: {
+                isAccept: true
+            }
+        })
+
+        return Response.json({message: "is accepted by admin successfully"})
+
+    } catch (err) {
+        return Response.json({message: `interval error server => ${err}`} , {
+            status: 500
+        })
+    }
+
+}
+
