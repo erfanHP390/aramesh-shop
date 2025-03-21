a057669913349e1a82da7d0dfe748164b05d2c4e
erfan hosseini <erfanhosseini768@yahoo.com>
2025-03-21 12:30:48 +0330

    delete swal text-ticket

--- src/components/templates/p-user/tickets/ticket/Ticket.js
+++ src/components/templates/p-user/tickets/ticket/Ticket.js
@@ -1,16 +1,8 @@
 "use client"
 import Link from "next/link";
 import styles from "./Ticket.module.css";
-import swal from "sweetalert";
 
 function Ticket({ _id, title, createdAt, department, subDepartment, hasAnswer, body }) {
-
-    const showBody = () => {
-        swal({
-            title: body,
-            buttons: "فهمیدم"
-        });
-    };
 
     return (
         <Link href={`/p-user/tickets/answer/${_id}`} className={styles.ticket}>
@@ -18,11 +10,6 @@ function Ticket({ _id, title, createdAt, department, subDepartment, hasAnswer, b
                 <p>{title}</p>
                 <p className={styles.department}>{department.title}</p>
                 <p className={styles.department}>{subDepartment.title}</p>
-            </div>
-            <div>
-                <button onClick={showBody} className={styles.no_answer}>
-                    مشاهده متن پیام
-                </button>
             </div>
             <div>
                 <p>{new Date(createdAt).toLocaleDateString("fa-IR")}</p>
