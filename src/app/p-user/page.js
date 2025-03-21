a1bedfb354f6bad26c3cab639661713a5bee644f
erfan hosseini <erfanhosseini768@yahoo.com>
2025-03-21 10:43:56 +0330

    update nums of Box

--- src/app/p-user/page.js
+++ src/app/p-user/page.js
@@ -8,24 +8,27 @@ import connectToDB from '@/configs/db'
 import { authUser } from '@/utils/authUserLink'
 import TicketModel from "@/models/Ticket"
 import WishListModel from "@/models/Wishlist"
+import CommentModel from "@/models/Comment"
 
 async function Index() {
 
   connectToDB()
   const user =await authUser()
-  const tickets = await TicketModel.find({user: user?._id}).limit(3).populate("department" , "title").sort().lean()
-  const wishlist = await WishListModel.find({user: user?._id})
-  const allTickets = await TicketModel.find({user: user?._id})
+  const tickets = await TicketModel.find({user: user._id}).limit(3).populate("department" , "title").sort().lean()
+
+  const comments = await CommentModel.find({username: user.name , email: user.email})
+  const wishlist = await WishListModel.find({user: user._id})
+  const allTickets = await TicketModel.find({user: user._id , isAnswer: false})
 
   return (
     <>
       <UserPanelLayout>
       <main>
         <section className={styles.boxes}>
-          <Box title="مجموع تیکت ها " value={allTickets?.length} />
-          <Box title="مجموع کامنت ها " value="0" />
+          <Box title="مجموع تیکت ها " value={allTickets.length} />
+          <Box title="مجموع کامنت ها " value={comments.length} />
           <Box title="مجموع سفارشات" value="2" />
-          <Box title="مجموع علاقه مندی ها" value={wishlist?.length} />
+          <Box title="مجموع علاقه مندی ها" value={wishlist.length} />
         </section>
         <section className={styles.contents}>
           <Tickets tickets={JSON.parse(JSON.stringify(tickets))} />
