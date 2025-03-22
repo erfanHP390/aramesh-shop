2b069b1bb19c3085c2dd674bf318bf76bed7bfbd
erfan hosseini <erfanhosseini768@yahoo.com>
2025-03-22 19:50:46 +0330

    create page basket-shop

---
+++ src/app/cart/page.js
@@ -0,0 +1,31 @@
+import Stepper from '@/components/modules/cart/stepper/Stepper'
+import Footer from '@/components/modules/Footer/Footer'
+import Navbar from '@/components/modules/navbar/Navbar'
+import Table from '@/components/templates/cart/table/Table'
+import styles from "@/styles/cart.module.css"
+
+function page() {
+  return (
+    <>
+    <Navbar />
+    <Stepper step="cart" />
+
+    <main className={styles.cart} data-aos="fade-up">
+      <Table />
+    </main>
+
+    {/* <div class={styles.cart_empty} data-aos="fade-up">
+              <TbShoppingCartX />
+              <p>سبد خرید شما در حال حاضر خالی است. </p>
+              <span>قبل از تسویه حساب، باید چند محصول را به سبد خرید خود اضافه کنید.</span>
+              <span>در صفحه "فروشگاه"، محصولات جالب زیادی خواهید یافت.</span>
+              <div>
+                  <Link href='/category'>بازگشت به فروشگاه</Link>
+              </div>
+          </div> */}
+    <Footer />
+  </>
+  )
+}
+
+export default page
