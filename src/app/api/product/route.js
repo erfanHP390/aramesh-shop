66882725be370fe5bf769e683c1e4f5f747923a0
erfan hosseini <erfanhosseini768@yahoo.com>
2025-03-24 17:28:21 +0330

    create route with method PUT for upload file

--- src/app/api/product/route.js
+++ src/app/api/product/route.js
@@ -1,5 +1,8 @@
 import connectToDB from "@/configs/db";
 import ProductModel from "@/models/Product"
+// import fs from "fs";
+import { writeFile } from "fs/promises";
+import path from "path";
 
 export async function POST(req) {
   connectToDB();
@@ -28,6 +31,38 @@ export async function POST(req) {
   }
 
 }
+
+
+export async function PUT(req) {
+
+  try {
+    const formData = await req.formData()
+    const img = formData.get("img")
+    
+  
+    if(!img) {
+      return Response.json({message: "product has not image"} , {
+        status: 400
+      })
+    }
+
+    const buffer = Buffer.from(await img.arrayBuffer());
+    const filename = Date.now() + img.name;
+
+    await writeFile(path.join(process.cwd() , "public/uploads/" + filename) , buffer)
+  
+    return Response.json({message: "file uploaded successfully"} , {
+      status: 201
+    })
+  } catch (err) {
+    return Response.json({message: `interval error server for receive product => ${err}`} , {
+      status: 500
+    })
+
+}
+
+}
+
 
 export async function GET(req) {
 
