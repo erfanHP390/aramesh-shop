c5c018c2b4666bf45784616bf0fffe8f465b2985
erfan hosseini <erfanhosseini768@yahoo.com>
2025-03-27 20:22:53 +0330

    create model for comment-blog

---
+++ models/CommentBlog.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const schema = mongoose.Schema({
+    
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    website: {
+        type: String,
+        default: "www.ir"
+    },
+    description: {
+        type: String,
+        required: true
+    },
+
+
+}, { timestamps: true });
+
+const model = mongoose.models.CommentBlog || mongoose.model("CommentBlog", schema);
+export default model
