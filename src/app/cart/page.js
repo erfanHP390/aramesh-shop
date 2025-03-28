32a77bb0223da37e9db984291656a2202751d5f4
erfan hosseini <erfanhosseini768@yahoo.com>
2025-03-28 19:47:53 +0330

    add authUser to navbar

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -3,11 +3,15 @@ import Footer from '@/components/modules/Footer/Footer'
 import Navbar from '@/components/modules/navbar/Navbar'
 import Table from '@/components/templates/cart/table/Table'
 import styles from "@/styles/cart.module.css"
+import { authUser } from '@/utils/authUserLink'
 
-function page() {
+async function page() {
+
+  const user = await authUser()
+
   return (
     <>
-    <Navbar />
+    <Navbar  isLogin={user ? true : false} />
     <Stepper step="cart" />
 
     <main className={styles.cart} data-aos="fade-up">
