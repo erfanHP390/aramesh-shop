5f79a84643653a2e41667e476e9336b51fe0022c
erfan hosseini <erfanhosseini768@yahoo.com>
2025-03-28 12:13:28 +0330

    create rout for create comments for blog

---
+++ src/app/api/blog/comment/route.js
@@ -0,0 +1,59 @@
+import connectToDB from "@/configs/db";
+import { validateEmail } from "@/utils/auth";
+import CommentBlogModel from "@/models/CommentBlog";
+import BlogModel from "@/models/Blog";
+
+export async function POST(req) {
+  try {
+    connectToDB();
+    const body = await req.json();
+    const { name, email, website, description, blogID } = body;
+
+    if (!name  || !email || !description || !blogID) {
+      return Response.json(
+        { message: "all fields must have something expect score" },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    const isValidEmail = validateEmail(email);
+    if (!isValidEmail) {
+      return Response.json(
+        { message: "email is not valid" },
+        {
+          status: 422,
+        }
+      );
+    }
+
+    const commentForBlog = CommentBlogModel.create({
+      name,
+      email,
+      website,
+      description,
+      blogID,
+    });
+
+    await BlogModel.findOneAndUpdate(
+      { _id: blogID },
+      {
+        $push: {
+          comments: commentForBlog._id,
+        },
+      }
+    );
+
+
+    return Response.json({message: "comment for blog is created successfully"} , {status: 201})
+
+  } catch (err) {
+    return Response.json(
+      { message: `interval error server ${err}` },
+      {
+        status: 500,
+      }
+    );
+  }
+}
