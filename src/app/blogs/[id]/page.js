0324d024f016819fe1d810dd601d4b06f050a8c9
erfan hosseini <erfanhosseini768@yahoo.com>
2025-03-28 11:43:44 +0330

    get articles from DB

--- src/app/blogs/[id]/page.js
+++ src/app/blogs/[id]/page.js
@@ -4,14 +4,21 @@ import Navbar from '@/components/modules/navbar/Navbar'
 import CommentArticle from '@/components/templates/blog/commentArticle/CommentArticle'
 import DetailsArticle from '@/components/templates/blog/detailsArticle/DetailsArticle'
 import styles from "@/styles/article.module.css"
+import BlogModel from "@/models/Blog"
 
-function page() {
+
+async function page({params}) {
+
+  const id = params.id 
+
+  const blog = await BlogModel.findOne({_id: id} , "-__v  -updatedAt").lean()
+
   return (
     <>
     <Navbar />
     <BreadCrumb route={'قهوه'} />
     <div className={styles.container}>
-        <DetailsArticle />
+        <DetailsArticle blog={JSON.parse(JSON.stringify(blog))} />
         <CommentArticle />
     </div>
 
