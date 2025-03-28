452f651ddb57006554f5800161b27437fad09662
erfan hosseini <erfanhosseini768@yahoo.com>
2025-03-28 11:42:47 +0330

    get articles form DB and backend

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -5,21 +5,19 @@ import Navbar from "@/components/modules/navbar/Navbar";
 import ArticleLoad from "@/components/templates/blog/articleLoad/ArticleLoad";
 import styles from "@/styles/articles.module.css";
 import { authUser } from "@/utils/authUserLink";
+import BlogModel from "@/models/Blog"
 
 async function page() {
-
   const user = await authUser()
+  const blogs = await BlogModel.find({} , "-__v  -updatedAt").lean()
 
   return (
     <>
       <Navbar isLogin={user ? true : false} />
       <BreadCrumb route={"اخبار و مقالات"} />
       <main className={styles.container}>
-        <div className={styles.articles}>
-          <ArticleLoad />
-        </div>
+        <ArticleLoad blogs={JSON.parse(JSON.stringify(blogs))} />
       </main>
-
       <Footer />
     </>
   );
