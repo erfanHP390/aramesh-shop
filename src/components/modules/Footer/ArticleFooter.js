c8dd5034497ae666a742f4ab82453d6149d681ce
erfan hosseini <erfanhosseini768@yahoo.com>
2025-03-29 12:15:41 +0330

    dynamic info of articles

--- src/components/modules/Footer/ArticleFooter.js
+++ src/components/modules/Footer/ArticleFooter.js
@@ -2,15 +2,15 @@ import React from 'react';
 import styles from "./ArticleFooter.module.css";
 import Link from 'next/link';
 
-export default function ArticleFooter({ href, img, title, comments, date }) {
+export default function ArticleFooter({ _id , img , title , comments , createdAt}) {
   return (
-    <Link href={href} className={styles.article}>
+    <Link href={`/blog/${_id}`} className={styles.article}>
       <img width={75} height={65} src={img} alt={title} />
       <div>
         <p className={styles.title}>{title}</p>
         <div>
-          <p>{comments}</p>
-          <p dir="rtl">{date}</p>
+          <p>دیدگاه({Number(comments.length).toLocaleString("fa-IR")})</p>
+          <p dir="rtl">{new Date(createdAt).toLocaleDateString("fa-IR")}</p>
         </div>
       </div>
     </Link>
