c9b7a76589288b78521039ac0985e5b1be60fc9b
erfan hosseini <erfanhosseini768@yahoo.com>
2025-04-05 23:18:05 +0330

    create route with method put-api to change details-comment

--- src/app/api/comment/[id]/route.js
+++ src/app/api/comment/[id]/route.js
@@ -1,6 +1,7 @@
 import connectToDB from "@/configs/db";
 import { isValidObjectId } from "mongoose";
-import CommentModel from "@/models/Comment"
+import CommentModel from "@/models/Comment";
+import { validateEmail } from "@/utils/auth";
 
 export async function DELETE(req, { params }) {
   try {
@@ -25,21 +26,91 @@ export async function DELETE(req, { params }) {
       );
     }
 
-      const comment = await CommentModel.findOne({_id: id})
+    const comment = await CommentModel.findOne({ _id: id });
 
-      if(!comment) {
-        return Response.json({message: "comment not found"} , {
-          status: 404
-        })
-      }
+    if (!comment) {
+      return Response.json(
+        { message: "comment not found" },
+        {
+          status: 404,
+        }
+      );
+    }
 
-      await CommentModel.findOneAndDelete({_id: id})
+    await CommentModel.findOneAndDelete({ _id: id });
 
-      return Response.json({message: "comment deleted successfully"})
-
+    return Response.json({ message: "comment deleted successfully" });
   } catch (err) {
     return Response.json(`interval error server ${err}`, {
       status: 500,
     });
+  }
+}
+
+export async function PUT(req, { params }) {
+  try {
+    connectToDB();
+    const id = params.id;
+
+    const reqBody = await req.json();
+    const { body, email, score, isAccept } = reqBody;
+
+    if (!body || !score) {
+      return Response.json(
+        { message: "all fields must be filled" },
+        { status: 400 }
+      );
+    }
+
+    const isValidEmail = validateEmail(email);
+    if (!isValidEmail) {
+      return Response.json(
+        { message: "email is not valid" },
+        { status: 422 }
+      );
+    }
+
+    if (!id) {
+      return Response.json(
+        { message: "id must be sent" },
+        { status: 400 }
+      );
+    }
+
+    if (!isValidObjectId(id)) {
+      return Response.json(
+        { message: "id is not valid" },
+        { status: 422 }
+      );
+    }
+
+    const comment = await CommentModel.findOne({ _id: id });
+    if (!comment) {
+      return Response.json(
+        { message: "comment is not found" },
+        { status: 404 }
+      );
+    }
+
+    await CommentModel.updateOne(
+      { _id: id },
+      {
+        $set: {
+          email,
+          body,
+          score: score !== undefined ? score : comment.score, // اگر score ارسال نشده از مقدار قبلی استفاده کن
+        },
+      }
+    );
+
+    return Response.json(
+      { message: "comment is updated successfully" },
+      { status: 200 }
+    );
+  } catch (err) {
+    return Response.json(
+      { message: `interval error server: ${err.message}` },
+      { status: 500 }
+    );
   }
 }
