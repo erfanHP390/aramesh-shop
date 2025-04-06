a648f2ccf3d16ffbcddc254833806a5dda0f9f51
erfan hosseini <erfanhosseini768@yahoo.com>
2025-04-06 21:30:17 +0330

    add profile-user

--- src/app/p-user/account-details/page.js
+++ src/app/p-user/account-details/page.js
@@ -1,11 +1,19 @@
 import UserPanelLayout from '@/components/layouts/UserPanelLayout/UserPanelLayout'
 import AccountDetail from '@/components/templates/p-user/accountDetail/AccountDetail'
+import connectToDB from '@/configs/db'
+import UserProfileModel from "@/models/UserProfile"
+import { authUser } from '@/utils/authUserLink'
 
 
-function page() {
+async function page() {
+
+  connectToDB()
+  const user =  await authUser()
+  const profileUser = await UserProfileModel.findOne({user: user._id})
+
   return (
     <UserPanelLayout>
-      <AccountDetail />
+      <AccountDetail  profileUser={JSON.parse(JSON.stringify(profileUser))} />
     </UserPanelLayout>
   )
 }
