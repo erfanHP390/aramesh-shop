ae9c6bb1e532562fa4a03b2f10d01ad7b6f02a87
erfan hosseini <erfanhosseini768@yahoo.com>
2025-04-12 22:50:56 +0330

    onClick and connection DB to delete blog

--- src/components/templates/p-admin/blogs/BlogTable.js
+++ src/components/templates/p-admin/blogs/BlogTable.js
@@ -1,22 +1,91 @@
-"use client"
-import Modal from '@/components/modules/Modal/Modal'
-import styles from "@/components/templates/p-admin/products/ProductTable.module.css"
-import Link from 'next/link'
-import { useRouter } from 'next/navigation'
-import React, { useState } from 'react'
+"use client";
+import Modal from "@/components/modules/Modal/Modal";
+import styles from "@/components/templates/p-admin/products/ProductTable.module.css";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { swalAlert, toastError, toastSuccess } from "@/utils/helpers";
 
-function BlogTable({blogs , title}) {
+function BlogTable({ blogs, title }) {
+  const router = useRouter();
+  const [showModal, setShowModal] = useState(false);
+  const [selectedBlog, setSelectedBlog] = useState(null);
+  const hideModal = () => setShowModal(false);
 
-    const router = useRouter()
-      const [showModal, setShowModal] = useState(false);
-      const [selectedBlog, setSelectedBlog] = useState(null);
-      const hideModal = () => setShowModal(false);
-    
-      const showBlogDetails = (blogID) => {
-        const blog = blogs.find(blog => blog._id === blogID);
-        setSelectedBlog(blog);
-        setShowModal(true);
+  const showBlogDetails = (blogID) => {
+    const blog = blogs.find((blog) => blog._id === blogID);
+    setSelectedBlog(blog);
+    setShowModal(true);
+  };
+
+  const removeBlog = async (blogID) => {
+    swal({
+      title: "آیا از حذف مقاله اطمینان دارید؟",
+      icon: "warning",
+      buttons: ["نه", "آره"],
+    }).then(async (result) => {
+      if (result) {
+        if (!blogID) {
+          return swalAlert("شناسه مقاله ارسال نشده است", "error", "فهمیدم");
+        }
+
+        const res = await fetch(`/api/blog/${blogID}`, {
+          method: "DELETE",
+        });
+
+        if (res.status === 200) {
+          toastSuccess(
+            "مقاله با موفقیت حذف شد",
+            "top-center",
+            5000,
+            false,
+            true,
+            true,
+            true,
+            undefined,
+            "colored"
+          );
+          router.refresh();
+        } else if (res.status === 400) {
+          toastError(
+            "شناسه مقالهمقاله ارسال نشده است",
+            "top-center",
+            5000,
+            false,
+            true,
+            true,
+            true,
+            undefined,
+            "colored"
+          );
+        } else if (res.status === 422) {
+          toastError(
+            "شناسه مقاله نامعتبر است",
+            "top-center",
+            5000,
+            false,
+            true,
+            true,
+            true,
+            undefined,
+            "colored"
+          );
+        } else if (res.status === 500) {
+          toastError(
+            "خطا در سرور ، لطفا بعدا تلاش کنید",
+            "top-center",
+            5000,
+            false,
+            true,
+            true,
+            true,
+            undefined,
+            "colored"
+          );
+        }
       }
+    });
+  };
 
   return (
     <>
@@ -29,7 +98,7 @@ function BlogTable({blogs , title}) {
         <div className={styles.table_container}>
           <table className={styles.table}>
             <thead>
-            <tr>
+              <tr>
                 <th>شناسه</th>
                 <th>عنوان</th>
                 <th>نویسنده</th>
@@ -48,9 +117,9 @@ function BlogTable({blogs , title}) {
                   <td>{blog.titr}</td>
 
                   <td>
-                    <button 
-                      onClick={() => showBlogDetails(blog._id)} 
-                      type="button" 
+                    <button
+                      onClick={() => showBlogDetails(blog._id)}
+                      type="button"
                       className={styles.edit_btn}
                     >
                       مشاهده جزئیات
@@ -62,7 +131,11 @@ function BlogTable({blogs , title}) {
                     </button>
                   </td>
                   <td>
-                    <button  type="button" className={styles.delete_btn}>
+                    <button
+                      onClick={() => removeBlog(blog._id)}
+                      type="button"
+                      className={styles.delete_btn}
+                    >
                       حذف
                     </button>
                   </td>
@@ -72,14 +145,19 @@ function BlogTable({blogs , title}) {
           </table>
         </div>
       </div>
-      
+
       {showModal && selectedBlog && (
-        <Modal title={`جزئیات مقاله ${selectedBlog.title}`} hideModal={hideModal}>
+        <Modal
+          title={`جزئیات مقاله ${selectedBlog.title}`}
+          hideModal={hideModal}
+        >
           <div className={styles.modal_product}>
             <div className={styles.modal_product_header}>
-              <h2 className={styles.modal_product_title}>{selectedBlog.title}</h2>
+              <h2 className={styles.modal_product_title}>
+                {selectedBlog.title}
+              </h2>
             </div>
-            
+
             <div className={styles.modal_product_content}>
               <div className={styles.modal_product_image_container}>
                 <img
@@ -89,65 +167,78 @@ function BlogTable({blogs , title}) {
                   className={styles.modal_product_image}
                 />
               </div>
-              
+
               <div className={styles.modal_product_info}>
-              <div className={styles.modal_product_meta}>
+                <div className={styles.modal_product_meta}>
                   <div className={styles.modal_product_meta_item}>
-                    <span className={styles.modal_product_meta_label}>نویسنده</span>
+                    <span className={styles.modal_product_meta_label}>
+                      نویسنده
+                    </span>
                     <span className={styles.modal_product_meta_value}>
-                      {selectedBlog.author} 
+                      {selectedBlog.author}
                     </span>
                   </div>
                 </div>
 
                 <div className={styles.modal_product_meta}>
                   <div className={styles.modal_product_meta_item}>
-                    <span className={styles.modal_product_meta_label}>تاریخ ایجاد</span>
+                    <span className={styles.modal_product_meta_label}>
+                      تاریخ ایجاد
+                    </span>
                     <span className={styles.modal_product_meta_value}>
-                      {new Date(selectedBlog.createdAt).toLocaleDateString("fa-IR")} 
+                      {new Date(selectedBlog.createdAt).toLocaleDateString(
+                        "fa-IR"
+                      )}
                     </span>
                   </div>
                 </div>
 
                 <div className={styles.modal_product_meta}>
                   <div className={styles.modal_product_meta_item}>
-                    <span className={styles.modal_product_meta_label}>آخرین بروزرسانی</span>
-                    <span className={styles.modal_product_meta_value}>
-                    {new Date(selectedBlog.updatedAt).toLocaleDateString("fa-IR")} 
+                    <span className={styles.modal_product_meta_label}>
+                      آخرین بروزرسانی
                     </span>
-                  </div>
-                </div>
-                
-                <div className={styles.modal_product_meta}>
-                  <div className={styles.modal_product_meta_item}>
-                    <span className={styles.modal_product_meta_label}>تیتر</span>
                     <span className={styles.modal_product_meta_value}>
-                      {selectedBlog.titr} 
+                      {new Date(selectedBlog.updatedAt).toLocaleDateString(
+                        "fa-IR"
+                      )}
                     </span>
                   </div>
                 </div>
 
+                <div className={styles.modal_product_meta}>
+                  <div className={styles.modal_product_meta_item}>
+                    <span className={styles.modal_product_meta_label}>
+                      تیتر
+                    </span>
+                    <span className={styles.modal_product_meta_value}>
+                      {selectedBlog.titr}
+                    </span>
+                  </div>
+                </div>
               </div>
-              
-              
-              
+
               <div className={styles.modal_product_description}>
-                <h3 className={styles.modal_product_description_title}>چکیده</h3>
+                <h3 className={styles.modal_product_description_title}>
+                  چکیده
+                </h3>
                 <p className={styles.modal_product_description_text}>
                   {selectedBlog.shortDesc}
                 </p>
               </div>
-              
+
               <div className={styles.modal_product_description}>
-                <h3 className={styles.modal_product_description_title}>مقاله</h3>
+                <h3 className={styles.modal_product_description_title}>
+                  مقاله
+                </h3>
                 <p className={styles.modal_product_description_text}>
                   {selectedBlog.description}
                 </p>
               </div>
             </div>
-            
+
             <div className={styles.modal_product_actions}>
-              <button 
+              <button
                 className={`${styles.modal_product_button} ${styles.modal_product_button_secondary}`}
                 onClick={hideModal}
               >
@@ -158,8 +249,7 @@ function BlogTable({blogs , title}) {
         </Modal>
       )}
     </>
-  )
+  );
 }
 
-export default BlogTable
-
+export default BlogTable;
