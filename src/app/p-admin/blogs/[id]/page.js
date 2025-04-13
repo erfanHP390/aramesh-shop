24670ce6f5a70b206d75ac163167f344cff43722
erfan hosseini <erfanhosseini768@yahoo.com>
2025-04-13 19:09:59 +0330

    create page for edit blogs

---
+++ src/app/p-admin/blogs/[id]/page.js
@@ -0,0 +1,22 @@
+import AdminPanelLayout from '@/components/layouts/AdminPanelLayout/AdminPanelLayout'
+import connectToDB from '@/configs/db'
+import React from 'react'
+import BlogModel from "@/models/Blog"
+import BlogDetails from '@/components/templates/p-admin/blogs/BlogDeatils'
+import { JsonWebTokenError } from 'jsonwebtoken'
+
+async function page({params}) {
+
+    connectToDB()
+    const id = params.id
+
+    const blog = await BlogModel.findOne({_id: id}).lean()
+
+  return (
+    <AdminPanelLayout>
+      <BlogDetails blog={JSON.parse(JSON.stringify(blog))}   />
+    </AdminPanelLayout>
+  )
+}
+
+export default page
