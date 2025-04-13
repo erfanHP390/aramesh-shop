70d8dbf92446d375a58b3d957e35edd26ad5cf56
erfan hosseini <erfanhosseini768@yahoo.com>
2025-04-13 22:17:38 +0330

    convert to server component

--- src/app/login&register/page.js
+++ src/app/login&register/page.js
@@ -1,33 +1,11 @@
-"use client";
-import styles from "@/styles/login&register.module.css";
-import { useState } from "react";
-import { authTypes } from "@/utils/constants";
-
-import Login from "@/components/templates/login&register/Login";
-import Register from "@/components/templates/login&register/Register";
+import LoginRegister from "@/components/templates/formPage/LoginRegister";
 
 const login_register = () => {
-  const [authType, setAuthType] = useState(authTypes.LOGIN);
-
-  const showRegisterForm = () => setAuthType(authTypes.REGISTER);
-  const showLoginForm = () => setAuthType(authTypes.LOGIN);
 
   return (
-    <div className={styles.login_register}>
-      <div className={styles.form_bg} data-aos="fade-up">
-        {authType === authTypes.LOGIN ? (
-          <Login showRegisterForm={showRegisterForm} />
-        ) : (
-          <Register showLoginForm={showLoginForm} />
-        )}
-      </div>
-      <section className={styles.login_img}>
-        <img
-          src="https://neurosciencenews.com/files/2023/06/coffee-brain-caffeine-neuroscincces.jpg"
-          alt=""
-        />
-      </section>
-    </div>
+    <>
+    <LoginRegister />
+    </>
   );
 };
 
