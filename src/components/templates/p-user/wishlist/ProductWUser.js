f13be55b7ecf3634a0e78fca7382ecd11c3dd2e0
erfan hosseini <erfanhosseini768@yahoo.com>
2025-04-16 21:38:49 +0330

    connection to DB and send to pai for remove products form wish-list

--- src/components/templates/p-user/wishlist/ProductWUser.js
+++ src/components/templates/p-user/wishlist/ProductWUser.js
@@ -1,17 +1,79 @@
 "use client"
+import { toastError, toastSuccess } from "@/utils/helpers";
 import styles from "./ProductWUser.module.css"
 import Link from 'next/link';
+import { useRouter } from "next/navigation";
 import { FaRegStar, FaStar } from 'react-icons/fa';
 
-function ProductWUser({ name, price, score, _id , img }) {
+function ProductWUser({ name, price, score, productID , wishID , img }) {
+    const router = useRouter()
 
-    const removeProduct = (productId) => {
+    const removeProduct = () => {
         swal({
             title: "آیا از حذف محصول اطمینان دارید؟",
             icon: "warning",
             buttons: ["نه", "آره"],
-        }).then((result) => {
-            //code
+        }).then(async (result) => {
+            if(result) {
+
+                const res = await fetch(`/api/whishlist/${wishID}` , {
+                    method: "DELETE"
+                })
+
+                
+                if (res.status === 200) {
+                    toastSuccess(
+                      "محصول با موفقیت از علاقه مندی شما حذف شد",
+                      "top-center",
+                      5000,
+                      false,
+                      true,
+                      true,
+                      true,
+                      undefined,
+                      "colored"
+                    );
+                    router.refresh();
+                } else if (res.status === 400) {
+                    toastError(
+                      "شناسه محصول ارسال نشده است",
+                      "top-center",
+                      5000,
+                      false,
+                      true,
+                      true,
+                      true,
+                      undefined,
+                      "colored"
+                    );
+                } else if (res.status === 401) {
+                  toastError(
+                    "احراز کاربر نامشخص است لطفا ابتدا وارد حساب خود شوید",
+                    "top-center",
+                    5000,
+                    false,
+                    true,
+                    true,
+                    true,
+                    undefined,
+                    "colored"
+                  );
+                  router.replace("/login&register")
+                } else if (res.status === 500) {
+                  toastError(
+                    "خطا در سرور ، لطفا بعدا تلاش کنید",
+                    "top-center",
+                    5000,
+                    false,
+                    true,
+                    true,
+                    true,
+                    undefined,
+                    "colored"
+                  );
+                }
+
+            }
         });
     };
 
@@ -38,7 +100,7 @@ function ProductWUser({ name, price, score, _id , img }) {
                 </div>
                 <span>{price?.toLocaleString()} تومان</span>
             </div>
-            <button onClick={() => removeProduct(_id)} className={styles.delete_btn}>
+            <button onClick={() => removeProduct()} className={styles.delete_btn}>
                 حذف محصول
             </button>
         </div>
