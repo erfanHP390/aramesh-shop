0cd193cf90a2b8c449819e5acf75e6e7f1adcbe2
erfan hosseini <erfanhosseini768@yahoo.com>
2025-04-18 22:36:52 +0330

    update entire data-site

--- src/app/p-admin/page.js
+++ src/app/p-admin/page.js
@@ -6,6 +6,9 @@ import TicketModel from "@/models/Ticket";
 import CommentModel from "@/models/Comment";
 import UserModel from "@/models/User";
 import ProductModel from "@/models/Product";
+import OrdersModel from "@/models/Orders"
+import CommentBlogsModel from "@/models/CommentBlog"
+import BlogModel from "@/models/Blog"
 import SaleChart from "@/components/templates/p-admin/index/saleChart/SaleChart";
 import GrowthChart from "@/components/templates/p-admin/index/growthChart/GrowthChart";
 
@@ -15,6 +18,10 @@ async function page() {
   const tickets = await TicketModel.find({}).lean()
   const products = await ProductModel.find({}).lean()
   const users = await UserModel.find({}).lean()
+  const orders = await OrdersModel.find({}).lean()
+  const comments = await CommentModel.find({}).lean()
+  const commentBlogs = await CommentBlogsModel.find({}).lean()
+  const blogs = await BlogModel.find({}).lean()
 
   return (
     <AdminPanelLayout>
@@ -22,8 +29,10 @@ async function page() {
         <section className={styles.dashboard_contents}>
           <Box title="مجموع تیکت های دریافتی" value={tickets.length} />
           <Box title="مجموع محصولات سایت" value={products.length} />
-          <Box title="مجموع سفارشات" value="333" />
+          <Box title="مجموع سفارشات" value={orders.length} />
           <Box title="مجموع کاربر های سایت" value={users.length} />
+          <Box title="مجموع مقالات سایت" value={blogs.length} />
+          <Box title="مجموع کامنت های سایت" value={Number(comments.length) + Number(commentBlogs.length)} />
         </section>{" "}
         <div className={styles.dashboard_charts}>
           <section>
