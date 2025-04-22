9ba95f444489795e1962d945662a3ee8143b17ea
erfan hosseini <erfanhosseini768@yahoo.com>
2025-04-22 21:33:06 +0330

    add comment-blogs in page

--- src/app/p-admin/comments/page.js
+++ src/app/p-admin/comments/page.js
@@ -3,33 +3,46 @@ import React from "react";
 import styles from "@/components/templates/p-admin/comments/CommentTable.module.css";
 import connectToDB from "@/configs/db";
 import CommentModel from "@/models/Comment";
+import CommentBlogModel from "@/models/CommentBlog";
 import CommentTable from "@/components/templates/p-admin/comments/CommentTable";
 import { authUser } from "@/utils/authUserLink";
+import CommentBTable from "@/components/templates/p-admin/comments/CommentBTable";
 
 const page = async () => {
-    connectToDB();
-    const comments = await CommentModel.find({})
-      .sort({ _id: -1 })
-      .populate("productID")
-      .lean();
-      const user = await authUser()
-  
-    return (
-      <AdminPanelLayout>
-        <main>
-          {comments.length === 0 ? (
-            <p className={styles.empty}>کامنتی وجود ندارد</p>
-          ) : (
+  connectToDB();
+  const comments = await CommentModel.find({})
+    .sort({ _id: -1 })
+    .populate("productID")
+    .lean();
+  const user = await authUser();
+
+  const commentsBlog = await CommentBlogModel.find({})
+    .sort({ _id: -1 })
+    .populate("blogID")
+    .lean();
+
+  return (
+    <AdminPanelLayout>
+      <main>
+        {comments.length === 0 && commentsBlog.length === 0 ? (
+          <p className={styles.empty}>کامنتی وجود ندارد</p>
+        ) : (
+          <>
             <CommentTable
               comments={JSON.parse(JSON.stringify(comments))}
-              title="لیست کامنت ها"
+              title="لیست کامنت محصولات"
               phone={user.phone}
             />
-          )}
-        </main>
-      </AdminPanelLayout>
-    );
-  };
-  
-  export default page;
-  
+            <CommentBTable
+              comments={JSON.parse(JSON.stringify(commentsBlog))}
+              title="لیست کامنت مقالات"
+              phone={user.phone}
+            />
+          </>
+        )}
+      </main>
+    </AdminPanelLayout>
+  );
+};
+
+export default page;
