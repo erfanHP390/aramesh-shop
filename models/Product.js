e5127250562f4037a6f2e75ce45850506048ee73
erfan hosseini <erfanhosseini768@yahoo.com>
2025-04-29 21:45:16 +0330

    add stock and uses in model

--- models/Product.js
+++ models/Product.js
@@ -1,63 +1,70 @@
-const mongoose = require("mongoose")
-require("./Comment")
+const mongoose = require("mongoose");
+require("./Comment");
 
-const schema = mongoose.Schema({
+const schema = mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     price: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     shortDesc: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     longDesc: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     weight: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     suitableFor: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     smell: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     score: {
-        type: Number,
-        default: 5
+      type: Number,
+      default: 5,
     },
     tags: {
-        type: [String],
-        required: true
+      type: [String],
+      required: true,
     },
     img: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     stock: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     uses: {
-        type: Number,
-        default: 0
+      type: Number,
+      default: 0,
     },
     comments: {
-        type: [{
-            type: mongoose.Types.ObjectId,
-            ref: "Comment"
-        }],
+      type: [
+        {
+          type: mongoose.Types.ObjectId,
+          ref: "Comment",
+        },
+      ],
     },
-})
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const model = mongoose.models.Product || mongoose.model("Product" , schema)
+const model = mongoose.models.Product || mongoose.model("Product", schema);
 
-export default model
+export default model;
