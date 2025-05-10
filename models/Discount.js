bc1db7786577e9ec35895e39ee49c653e92169b0
erfan hosseini <erfanhosseini768@yahoo.com>
2025-05-10 21:25:25 +0330

    add user-id for uses-once discount

--- models/Discount.js
+++ models/Discount.js
@@ -1,25 +1,29 @@
 const mongoose = require("mongoose")
+require("./User")
 
 const schema = mongoose.Schema({
+  code: {
+    type: String,
+    required: true
+  },
+  percent: {
+    type: Number,
+    required: true
+  },
+  maxUse: {
+    type: Number,
+    required: true
+  },
+  uses: {
+    type: Number,
+    default: 0
+  },
+  usedBy: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: 'User', 
+    default: []
+  }
+}, {timestamps: true});
 
-    code: {
-        type: String,
-        required: true
-    },
-    percent: {
-        type: Number,
-        required: true
-    },
-    maxUse: {
-        type: Number,
-        required: true
-    },
-    uses: {
-        type: Number,
-        default: 0
-    },
-
-} , {timestamps: true})
-
-const model = mongoose.models.Discount || mongoose.model("Discount" , schema)
-export  default model
+const model = mongoose.models.Discount || mongoose.model("Discount", schema);
+export default model;
