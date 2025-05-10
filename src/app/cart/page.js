c34c2cfc6790a7f5f35d34a9e3806cc00ffbe837
erfan hosseini <erfanhosseini768@yahoo.com>
2025-05-10 21:25:49 +0330

    add user-ID

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -2,16 +2,20 @@ import Stepper from "@/components/modules/cart/stepper/Stepper";
 import Footer from "@/components/modules/Footer/Footer";
 import Navbar from "@/components/modules/navbar/Navbar";
 import Table from "@/components/templates/cart/table/Table";
+import connectToDB from "@/configs/db";
 import styles from "@/styles/cart.module.css";
+import { authUser } from "@/utils/authUserLink";
 
 async function page() {
+  connectToDB()
+  const user = await authUser()
   return (
     <>
       <Navbar />
       <Stepper step="cart" />
 
       <main className={styles.cart} data-aos="fade-up">
-        <Table />
+        <Table userID={JSON.parse(JSON.stringify(user._id))} />
       </main>
       <Footer />
     </>
